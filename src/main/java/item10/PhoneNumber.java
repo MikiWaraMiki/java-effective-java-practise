dd50f8f6622f31c26d1858b02f143d5415563363
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2022-04-10 10:54:41 +0900

    add 項目10 equals

---
+++ src/main/java/item10/PhoneNumber.java
@@ -0,0 +1,21 @@
+package item10;
+
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class PhoneNumber {
+    private final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "area code");
+        this.prefix = rangeCheck(prefix, 999, "prefix");
+        this.lineNum = rangeCheck(lineNum, 999, "arg");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max) {
+            throw new IllegalArgumentException(arg + ": " + val);
+        }
+        return (short) val;
+    }
+}
---
+++ src/test/java/item10/PhoneNumberTest.java
@@ -0,0 +1,43 @@
+package item10;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneNumberTest {
+    @Nested
+    @DisplayName("等価テスト")
+    class EqualsTest {
+        @Test
+        void TestEqualsSameAreaCodeAndPrefixAndLineNum() {
+            val phoneNumber1 = new PhoneNumber(111, 111, 111);
+            val phoneNumber2 = new PhoneNumber(111, 111, 111);
+
+            assertEquals(phoneNumber1, phoneNumber2);
+        }
+        @Test
+        void TestNotEqualsDifferentAreaCode() {
+            val phoneNumber1 = new PhoneNumber(111, 111, 111);
+            val phoneNumber2 = new PhoneNumber(112, 111, 111);
+
+            assertNotEquals(phoneNumber1, phoneNumber2);
+        }
+        @Test
+        void TestNotEqualsDifferentPrefix() {
+            val phoneNumber1 = new PhoneNumber(111, 111, 111);
+            val phoneNumber2 = new PhoneNumber(111, 112, 111);
+
+            assertNotEquals(phoneNumber1, phoneNumber2);
+        }
+        @Test
+        void TestNotEqualsDifferentLineNum() {
+            val phoneNumber1 = new PhoneNumber(111, 111, 111);
+            val phoneNumber2 = new PhoneNumber(111, 111, 112);
+
+            assertNotEquals(phoneNumber1, phoneNumber2);
+        }
+    }
+}
